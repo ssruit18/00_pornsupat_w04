47a8b1c553aba9c5db1511d5ca791f8925eab83e
ssruit18 <s62822202004@ssru.ac.th>
2025-06-24 10:37:30 +0700

    24-06-25

---
+++ while-loop/02_break.py
@@ -0,0 +1,13 @@
+numbers = [12,15,63,21,24,30,36,106,48]
+divisor = 6
+
+print(f"ตัวที่หาร {divisor} ลงตัว :")
+found = 0
+
+for i in numbers:
+    if i % divisor == 0:
+        print(f"{i} หาร {divisor} ตัวที่หารลงตัวคือ({i // divisor})")
+        found += 1
+        
+        if found == 3:
+            break
